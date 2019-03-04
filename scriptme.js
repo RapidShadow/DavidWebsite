891f6caaa8b3382c8900f7b87f03854775be000b
David Ung <davidu@csedge.org>
2019-03-03 22:49:36 -0800

    Initial commit

---
+++ scriptme.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+$("#SpellBreak").hover(enterSpell, leaveSpell);
+function enterSpell(){
+  this.src="images/SpellBreak2.gif"
+}
+function leaveSpell(){
+  this.src="images/SpellBreak1.jpg"
+}
+});
